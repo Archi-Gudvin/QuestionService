4bb7d79573f2f6a8c25f5e98c64f258b4ba23396
dotneteeer <447599489marat@gmail.com>
2026-08-14 14:40:38 +0300

    refactor: remove LoggerFixture and update MapperFixture to use correct MapperConfiguration

--- QuestionService.Tests/UnitTests/Fixtures/MapperFixture.cs
+++ QuestionService.Tests/UnitTests/Fixtures/MapperFixture.cs
@@ -7,7 +7,7 @@ internal static class MapperFixture
 {
     public static IMapper GetMapperConfiguration()
     {
-        var mockMapper = new AutoMapper.MapperConfiguration(cfg => cfg.AddMaps(typeof(QuestionMapping)));
+        var mockMapper = new MapperConfiguration(cfg => cfg.AddMaps(typeof(QuestionMapping)));
         return mockMapper.CreateMapper();
     }
 }
